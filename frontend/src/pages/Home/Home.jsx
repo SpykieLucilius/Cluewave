20642a03d06ef6c62328ae9bfd5e00533bd21346
Spykie Lucilius <spykie.lucilius@pm.me>
2025-10-14 02:18:44 +0300

    Simple Homepage for visualisation

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,18 @@
+import '../../styles/Home.css'
+
 export default function Home() {
   return (
-    <main style={{padding: 24, fontFamily: 'sans-serif'}}>
-      <h1>Cluewave</h1>
+    <main>
+        <div className="home-container">
+            <h1 className="home-title">Cluewave</h1>
+                <div className="home-buttons">
+                <button className="home-button">Play with friends</button>
+                <button className="home-button">Join a game</button>
+            </div>
+        </div>
+        <footer className="home-footer">
+            <p>2025 Cluewave. Open-source online party game project. Inspired by Wavelenght. Website under construction.</p>
+        </footer>
     </main>
   )
 }
