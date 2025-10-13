60ad4d804382dff013f03bb3e9b0861b4ee36c58
Spykie Lucilius <spykie.lucilius@pm.me>
2025-10-14 02:19:53 +0300

    change app entry point to be on Home

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
+import MainLayout from './layout/MainLayout.jsx'
 import Home from './pages/Home/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
-  </StrictMode>,
+    <MainLayout>
+      <Home />
+    </MainLayout>
+  </StrictMode>
 )
