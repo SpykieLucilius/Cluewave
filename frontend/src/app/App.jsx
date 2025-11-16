04e0884cab3ad768d5bbd0bc9366773d7aaa3e4a
Spykie Lucilius <spykie.lucilius@pm.me>
2025-11-16 17:28:15 +0200

    permission fix

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,41 +1,37 @@
 // frontend/src/app/App.jsx
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from "../pages/Home/Home.jsx";
-import Login from "../pages/Login/Login.jsx";
-import Register from "../pages/Register/Register.jsx";
-import NotFound from "../pages/NotFound.jsx";
+import Home from '../pages/Home/Home.jsx';
+import Login from '../pages/Login/Login.jsx';
+import Register from '../pages/Register/Register.jsx';
+import Profile from '../pages/Profile/Profile.jsx';
+import NotFound from '../pages/NotFound.jsx';
 
-import { AuthProvider } from "../context/auth/AuthContext.jsx";
-import { MusicProvider } from "../context/audio/MusicProvider.jsx";
-import MainLayout from "../layout/MainLayout.jsx";
+import { AuthProvider } from '../context/auth/AuthContext.jsx';
+import MainLayout from '../layout/MainLayout.jsx';
 
-import CreateRoom from "../pages/CreateRoom/CreateRoom.jsx";
-import JoinRoom from "../pages/JoinRoom/JoinRoom.jsx";
+import CreateRoom from '../pages/CreateRoom/CreateRoom.jsx';
+import JoinRoom from '../pages/JoinRoom/JoinRoom.jsx';
 
-const App = () => {
+export default function App() {
   return (
     <AuthProvider>
-      <MusicProvider>
-        <BrowserRouter>
-          <MainLayout>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
+      <BrowserRouter>
+        <MainLayout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
 
-              {/* nouvelles pages jeu */}
-              <Route path="/create" element={<CreateRoom />} />
-              <Route path="/join" element={<JoinRoom />} />
+            <Route path="/create" element={<CreateRoom />} />
+            <Route path="/join" element={<JoinRoom />} />
 
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </MainLayout>
-        </BrowserRouter>
-      </MusicProvider>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </MainLayout>
+      </BrowserRouter>
     </AuthProvider>
   );
-};
-
-export default App;
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app/App.jsx'
-import './styles/index.css'
+// frontend/src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/App.jsx';
+import './styles/index.css';
+
+import { SoundProvider } from './context/audio/SoundProvider.jsx';
+import { MusicProvider } from './context/audio/MusicProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <SoundProvider>
+      <MusicProvider>
+        <App />
+      </MusicProvider>
+    </SoundProvider>
+  </React.StrictMode>
+);
