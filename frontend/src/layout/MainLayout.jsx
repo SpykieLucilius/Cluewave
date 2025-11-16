074d95a9711cdbd10f317a160fc6875ad58fc0bc
Spykie Lucilius <spykie.lucilius@pm.me>
2025-11-16 19:41:42 +0200

    fix sound control

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -1,14 +1,10 @@
 import NavBar from '../components/NavBar/NavBar.jsx';
-import { MusicProvider } from '../context/audio/MusicProvider.jsx';
-import { SoundProvider } from '../context/audio/SoundContext.jsx';
 
 export default function MainLayout({ children }) {
     return (
-        <SoundProvider>
-        <MusicProvider>
+        <>
             <NavBar />
-                {children}
-            </MusicProvider>
-        </SoundProvider>
+            {children}
+        </>
     );
 }
